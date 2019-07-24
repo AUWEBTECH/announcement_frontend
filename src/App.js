d8bcc287de45497ac8893a123cccae03a3c54a87
AUWEBTECH <auwebtech@gmail.com>
2019-07-24 22:33:09 +0300

    Signin page task

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../src/style.css';
+import Sign from './Sign'
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Sign />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Sign.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import { Container,Header, Input, Grid,Checkbox, Button } from 'semantic-ui-react';
+
+class Sign extends Component{
+    render() {
+        return (
+            <div className="sign">
+                <Container className="container" textAlign='justified'>
+                    <Grid className="generalGrid"> 
+                        <Grid.Column width={16}>
+                            <Header textAlign='center' as='h2' className="headerSign">DUYURU SİSTEMİ</Header>
+                        </Grid.Column>
+                        <Grid.Column textAlign="center" width={16}>
+                                <Grid.Column width={16}>
+                                    <Input icon='user' className="w50" iconPosition='left' placeholder='Your ID' type="text"/>
+                                </Grid.Column>
+                                <Grid.Column width={16} className="mt20">
+                                    <Input icon='eye' className="w50" iconPosition='left' placeholder='Your Password' type="password"/>
+                                </Grid.Column>
+                        </Grid.Column>
+                            <Grid.Column floated='left' width={8}>
+                                <Checkbox label='REMEMBER ME' />
+                            </Grid.Column>
+                            <Grid.Column width={8}>
+                                <a href="#" className="lostPassword">Lost Your Password?</a>
+                            </Grid.Column>
+                    </Grid>
+                    
+                    <Grid textAlign="center" className="loginButton">    
+                        <Button color='blue'>Log In</Button>
+                    </Grid>
+                        
+                </Container>
+            </div>
+        )
+    }
+}
+
+export default Sign;
